7a99e227e14f197c8442f0e70d222f8205898d67
Tony Arcieri <tony.arcieri@gmail.com>
2012-03-26 20:42:48 -0700

    Get specs to actually run

--- spec/nio/buffer_spec.rb
+++ spec/nio/buffer_spec.rb
@@ -1,8 +1,11 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/iobuffer')
+require 'spec_helper'
 
-describe IO::Buffer do
+# TODO: this should come in via native extension
+class NIO::Buffer; end
+
+describe NIO::Buffer do
   before :each do
-    @buffer = IO::Buffer.new
+    @buffer = NIO::Buffer.new
     @buffer.size.should == 0
   end
 
@@ -99,28 +102,28 @@ describe IO::Buffer do
   end
 
   it "can set default node size" do
-    IO::Buffer.default_node_size = 1
-    IO::Buffer.default_node_size.should == 1
-    (IO::Buffer.default_node_size = 4096).should == 4096
-    (IO::Buffer.default_node_size = IO::Buffer::MAX_SIZE).should == IO::Buffer::MAX_SIZE
+    NIO::Buffer.default_node_size = 1
+    NIO::Buffer.default_node_size.should == 1
+    (NIO::Buffer.default_node_size = 4096).should == 4096
+    (NIO::Buffer.default_node_size = NIO::Buffer::MAX_SIZE).should == NIO::Buffer::MAX_SIZE
   end
 
   it "can be created with a different node size" do
-    IO::Buffer.new(16384)
+    NIO::Buffer.new(16384)
   end
 
   it "cannot set invalid node sizes" do
     proc {
-      IO::Buffer.default_node_size = IO::Buffer::MAX_SIZE + 1
+      NIO::Buffer.default_node_size = NIO::Buffer::MAX_SIZE + 1
     }.should raise_error(ArgumentError)
     proc {
-      IO::Buffer.default_node_size = 0
+      NIO::Buffer.default_node_size = 0
     }.should raise_error(ArgumentError)
     proc {
-      IO::Buffer.new(IO::Buffer::MAX_SIZE + 1)
+      NIO::Buffer.new(NIO::Buffer::MAX_SIZE + 1)
     }.should raise_error(ArgumentError)
     proc {
-      IO::Buffer.new(0)
+      NIO::Buffer.new(0)
     }.should raise_error(ArgumentError)
   end
 
